7601cc77dd4b975ffe61593d9bde46a3a63f335e
Half1900 <2431413142@qq.com>
2023-11-12 23:10:36 +0800

    添加项目文件。

---
+++ ChangeHotkey.xaml.cs
@@ -0,0 +1,172 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace AlphaClicker
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class ChangeHotkey: Window
+    {
+        public ChangeHotkey()
+        {
+            InitializeComponent();
+        }
+
+
+        private void hotkeyWindow_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void closeButton_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Close();
+        }
+
+        private string CodeToSpecialKey(int key)
+        {
+            switch (key)
+            {
+                case 27:
+                    return "ESC";
+                case 35:
+                    return "END";
+                case 36:
+                    return "HOME";
+                case 112:
+                    return "F1";
+                case 113:
+                    return "F2";
+                case 114:
+                    return "F3";
+                case 115:
+                    return "F4";
+                case 116:
+                    return "F5";
+                case 117:
+                    return "F6";
+                case 118:
+                    return "F7";
+                case 119:
+                    return "F8";
+                case 120:
+                    return "F9";
+                case 121:
+                    return "F10";
+                case 122:
+                    return "F11";
+                case 123:
+                    return "F12";
+                case 145:
+                    return "SCRLK";
+            }
+            return "";
+        }
+
+        private bool hasSpecKey = false;
+        private int key1 = -1;
+        private int key2 = -1;
+        private void hotkeyWindow_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            // Not good way of doing it, but it works.
+            if (keyBox.Text == "请按键")
+            {
+                keyBox.Clear();
+            }
+
+            if (!startBtn.IsEnabled)
+            {
+                for (int i = 8; i < 123; i++)
+                {
+                    if (WinApi.GetAsyncKeyState(i) > 0)
+                    {
+                        /* If Char Or Number */
+                        if (i >= 48 && i <= 90)
+                        {
+                            keyBox.AppendText(((char)i).ToString());
+                            startBtn.IsEnabled = true;
+                            key2 = i;
+                            okBtn.IsEnabled = true;
+                            break;
+                        }
+                        /* If function key */
+                        else if (i >= 112 && i <= 145)
+                        {
+                            keyBox.AppendText(CodeToSpecialKey(i));
+                            startBtn.IsEnabled = true;
+                            key2 = i;
+
+                            okBtn.IsEnabled = true;
+                            break;
+                        }
+                        else if (i == 27 || (i >= 35 && i <= 36))
+                        {
+                            keyBox.AppendText(CodeToSpecialKey(i));
+                            startBtn.IsEnabled = true;
+                            key2 = i;
+
+                            okBtn.IsEnabled = true;
+                            break;
+                        }
+                        /* If any extra keys (shift, ctrl, alt) */
+                        else if (i >= 16 && i <= 18)
+                        {
+                            if (hasSpecKey == false)
+                            {
+                                keyBox.AppendText(e.Key.ToString().Replace("左键", "")
+                                    .Replace("右键", "") + " + ");
+                                key1 = i;
+                                hasSpecKey = true;
+                            }
+                        }
+                    }
+
+
+                }
+            }
+        }
+
+        private void startBtn_Click(object sender, RoutedEventArgs e)
+        {
+            okBtn.IsEnabled = false;
+            hasSpecKey = false;
+            startBtn.IsEnabled = false;
+            keyBox.Text = "请按键";
+        }
+
+        private void okBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Keybinds.key1 = key1;
+            Keybinds.key2 = key2;
+            Keybinds.keyBinding = keyBox.Text;
+            AlphaRegistry.SetKeybindValues();
+
+            ((MainWindow)this.Owner).LoadKeybind();
+            Close();
+        }
+
+        private void cancelBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void hotkeyWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            keyBox.Text = Keybinds.keyBinding;
+        }
+
+        private void hotkeyWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            ((MainWindow)this.Owner).keyEnabled = true;
+        }
+    }
+
+    public class Keybinds
+    {
+        // Default keybinds if reg fail
+        public static int key1 = -1;
+        public static int key2 = (int)VK.VK_ESCAPE;
+        public static string keyBinding = "ESC"; 
+    }
+}
---
+++ WinApi.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows;
+
+namespace AlphaClicker
+{
+    public class WinApi
+    {
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct POINT
+        {
+            public int X;
+            public int Y;
+
+            public static implicit operator Point(POINT point)
+            {
+                return new Point(point.X, point.Y);
+            }
+        }
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint cButtons, uint dwExtraInfo);
+
+        [DllImport("user32.dll")]
+        public static extern int GetAsyncKeyState(int character);
+
+        [DllImport("user32.dll")]
+        public static extern int SetCursorPos(int x, int y);
+
+        // Get custom coords
+        [DllImport("user32.dll")]
+        public static extern bool GetCursorPos(out POINT lpPoint);
+
+
+
+
+
+
+        [DllImport("user32.dll")]
+        static extern IntPtr WindowFromPoint(POINT Point);
+
+        [DllImport("user32.dll")]
+        static extern bool GetWindowRect(IntPtr hWnd, out RECT lpRect);
+
+
+
+        // 导入 PostMessage 函数
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern IntPtr SendMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
+
+        [StructLayout(LayoutKind.Sequential)]
+        struct RECT
+        {
+            public int Left;
+            public int Top;
+            public int Right;
+            public int Bottom;
+        }
+
+        // 定义常量
+        const uint WM_LBUTTONDOWN = 0x0201;
+        const uint WM_LBUTTONUP = 0x0202;
+        const uint WM_RBUTTONDOWN = 0x0204;
+        const uint WM_RBUTTONUP = 0x0205;
+
+        const uint WM_MBUTTONDOWN = 0x0207;
+        const uint WM_MBUTTONUP = 0x0208;
+
+        const int MK_LBUTTON = 0x0001;
+
+        // 定义辅助函数
+        static int MAKELONG(int low, int high)
+        {
+            return (high << 16) | (low & 0xffff);
+        }
+
+
+
+        private static IntPtr hWnd;
+
+        /// <summary>
+        /// 发送窗口的点击位置消息
+        /// </summary>
+        private static IntPtr lParam;
+
+        private readonly static IntPtr wParam = new IntPtr(MK_LBUTTON);
+
+        private static int x, y;
+
+        public static Point GetCursorPosition()
+        {
+            POINT lpPoint;
+            GetCursorPos(out lpPoint);
+            return lpPoint;
+        }
+
+        public static void DoClick(string button, bool useCustomCoords, int X, int Y,bool bg)
+        {
+            if (useCustomCoords)
+            {
+                if (bg)
+                {
+                    if (x != X || y != Y)
+                    {
+                        hWnd = WindowFromPoint(new POINT { X = X, Y = Y });
+                        x = X;
+                        y = Y;
+
+
+                        RECT rect;
+                        GetWindowRect(hWnd, out rect);
+
+                        X -= rect.Left;
+                        Y -= rect.Top;
+                        lParam = new IntPtr(MAKELONG(X, Y));
+
+                    }
+                    switch (button)
+                    {
+                        case "左键":
+                            SendMessage(hWnd, WM_LBUTTONDOWN, IntPtr.Zero, lParam);
+                            SendMessage(hWnd, WM_LBUTTONUP, IntPtr.Zero, lParam);
+                            break;
+                        case "右键":
+                            SendMessage(hWnd, WM_RBUTTONDOWN, IntPtr.Zero, lParam);
+                            SendMessage(hWnd, WM_RBUTTONUP, IntPtr.Zero, lParam);
+                            break;
+                        case "中键":
+                            SendMessage(hWnd, WM_MBUTTONDOWN, IntPtr.Zero, lParam);
+                            SendMessage(hWnd, WM_MBUTTONUP, IntPtr.Zero, lParam);
+                            break;
+                    }
+                    //SendMessage(hWnd, WM_LBUTTONDOWN, IntPtr.Zero, lParam);
+                    //SendMessage(hWnd, WM_LBUTTONUP, IntPtr.Zero, lParam);
+                }
+                else
+                {
+                    SetCursorPos(X, Y);
+                    switch (button)
+                    {
+                        case "左键":
+                            mouse_event((uint)MOUSEEVENTF.LEFTDOWN | (uint)MOUSEEVENTF.LEFTUP, 0, 0, 0, 0);
+                            break;
+                        case "右键":
+                            mouse_event((uint)MOUSEEVENTF.RIGHTDOWN | (uint)MOUSEEVENTF.RIGHTUP, 0, 0, 0, 0);
+                            break;
+                        case "中键":
+                            mouse_event((uint)MOUSEEVENTF.MIDDLEDOWN | (uint)MOUSEEVENTF.MIDDLEUP, 0, 0, 0, 0);
+                            break;
+                    }
+                }
+
+            }
+            else
+            {
+                switch (button)
+                {
+                    case "左键":
+                        mouse_event((uint)MOUSEEVENTF.LEFTDOWN | (uint)MOUSEEVENTF.LEFTUP, 0, 0, 0, 0);
+                        break;
+                    case "右键":
+                        mouse_event((uint)MOUSEEVENTF.RIGHTDOWN | (uint)MOUSEEVENTF.RIGHTUP, 0, 0, 0, 0);
+                        break;
+                    case "中键":
+                        mouse_event((uint)MOUSEEVENTF.MIDDLEDOWN | (uint)MOUSEEVENTF.MIDDLEUP, 0, 0, 0, 0);
+                        break;
+                }
+            }
+             
+        }
+    }
+}
